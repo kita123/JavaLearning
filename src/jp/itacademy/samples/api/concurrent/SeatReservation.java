1a26c4a118f61695f0705e59cb291345df269225
itacademy <lets@itacademy.jp>
2012-07-14 21:39:06 +0900

    I/O

---
+++ src/jp/itacademy/samples/api/concurrent/SeatReservation.java
@@ -0,0 +1,13 @@
+package jp.itacademy.samples.api.concurrent;
+
+import javax.swing.text.StyledEditorKit.AlignmentAction;
+
+public class SeatReservation {
+public static void main(String[] args) {
+	
+	Seat seat=new Seat();
+	
+	Person alice=new Person("Alice",seat);
+	new Thread()
+}
+}
