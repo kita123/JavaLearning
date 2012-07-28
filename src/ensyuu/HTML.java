090f7681b35628d8aa29d66e0c9296a314dbcb8c
itacademy <lets@itacademy.jp>
2012-07-28 21:34:05 +0900

    ensyuu2

--- src/ensyuu/HTML.java
+++ src/ensyuu/HTML.java
@@ -1,7 +1,16 @@
 package ensyuu;
 
+import java.util.Scanner;
+
 public class HTML {
 public static void main(String[] args) {
+
+	Scanner scanner=new Scanner(System.in);
+	String  h=scanner.nextLine();
 	
+
+			System.out.println("<img src=\"img/"+h+".gif\">");
+		
+		}
 }
-}
+
